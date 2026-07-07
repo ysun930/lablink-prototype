d7d43261d3f45e3a9a734866c89e14d45fea3470
ysun930 <144362081qq@gmail.com>
2026-07-07 12:00:11 -0700

    Add cosineSimilarity function to calculate similarity

--- scripts/embedding.js
+++ scripts/embedding.js
@@ -15,3 +15,23 @@ async function getEmbedding(text) {
   const data = await response.json();
   return data.embeddings[0];
 }
+function cosineSimilarity(vecA, vecB) {
+  let dot = 0;
+  let magA = 0;
+  let magB = 0;
+
+  for (let i = 0; i < vecA.length; i++) {
+    dot += vecA[i] * vecB[i];
+    magA += vecA[i] * vecA[i];
+    magB += vecB[i] * vecB[i];
+  }
+
+  magA = Math.sqrt(magA);
+  magB = Math.sqrt(magB);
+
+  if (magA === 0 || magB === 0) {
+    return 0;
+  }
+
+  return dot / (magA * magB);
+}
